7ba90b5eedd78ce642ab25f178448dfc5fd8575d
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-19 19:42:30 +0400

    renderButton & renderLink reusable code

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import Button from "./components/Button";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
-      </div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">Something</Button>
+      </div>
       <ButtonGradient />
     </>
   );
