df6bedbcf2c3386400c621299e3d8a792000ad99
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-20 09:28:29 +0400

    Section update

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -10,6 +10,7 @@ const Section = ({
 }) => {
   return (
     <div
+      id={id}
       className={`relative ${
         customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
