ea24beacb1f18bf72bd6a866555bde1db7a22d52
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-25 20:14:24 +0400

    Heading update

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,3 +1,4 @@
+import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 
@@ -5,8 +6,13 @@ const Benefits = () => {
   return (
     <Section id="features">
       <div className="container relative z-2">
-        <Heading />
+        <Heading
+          className="md:max-w-mg lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
       </div>
+
+      <div className="flex flex-wrap gap-10 mb-10">{benefits}</div>
     </Section>
   );
 };
