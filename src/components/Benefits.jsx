43250587f915feb7fb3fd09657789d4ac622db33
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-25 23:53:32 +0400

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -12,7 +12,19 @@ const Benefits = () => {
         />
       </div>
 
-      <div className="flex flex-wrap gap-10 mb-10">{benefits}</div>
+      <div className="flex flex-wrap gap-10 mb-10">
+        {benefits.map((item) => (
+          <div
+            className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+            key={item.id}
+          >
+            <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+              <h5 className="h2 mb-5">{item.title}</h5>
+              <p className="body-2 mb-6 text-n-3">{item.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </Section>
   );
 };
