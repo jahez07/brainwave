5cc44c5c4891b4c6959536ff1fd6004abb010b9f
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-26 08:37:00 +0400

    Circular wrapper

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,5 +1,5 @@
 import { check } from "../assets";
-import { collabContent } from "../constants";
+import { collabContent, collabText } from "../constants";
 import Section from "./Section";
 import Button from "./Button";
 
@@ -26,6 +26,18 @@ const Collaboration = () => {
           </ul>
 
           <Button>Try it now</Button>
+        </div>
+
+        <div className="lg:ml-auto el:w-[38rem] mt-4">
+          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+            {collabText}
+          </p>
+
+          <div
+            className="relative left-1/2 flex w-[22rem] 
+            aspect-square border border-n-6 
+          rounded-full -translate-x-1/2 scale-75 md:scale-100"
+          ></div>
         </div>
       </div>
     </Section>
