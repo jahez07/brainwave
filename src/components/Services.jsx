bd8a99d7cf2bb861b5aaf5c14a5c1520aeb9c27f
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-26 13:41:03 +0400

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,17 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+          title="Generative AI made for creators."
+          text="Brainwave unlocks the potential of AI powered applications"
+        />
+      </div>
+    </Section>
+  );
 };
 
 export default Services;
