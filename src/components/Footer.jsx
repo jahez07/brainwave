dd3c044ad1bac1b509d155b5f3bdf37bc8481f7b
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-28 00:28:49 +0400

    Update Footer.jsx
    Crosses + Full Year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import Section from "./Section";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Section crosses className="!px-0!py-10">
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p>{new Date().getFullYear()} All rights reserved.</p>
+      </div>
+    </Section>
+  );
 };
 
 export default Footer;
