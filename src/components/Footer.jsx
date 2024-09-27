f52fe3d943843a95c4da3053d7a5a3ccbe825198
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-28 00:31:56 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ const Footer = () => {
   return (
     <Section crosses className="!px-0!py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
-        <p>© {new Date().getFullYear()}. All rights reserved.</p>
+        <p className="caption text-n-4 lg:block">
+          © {new Date().getFullYear()}. All rights reserved.
+        </p>
+
+        <ul className="flex gap-5 flex-wrap"></ul>
       </div>
     </Section>
   );
